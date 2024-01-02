5a5e0c6670799d24ca4f5d85efc9a3996e936317
soojin <pofoxz@gmail.com>
2024-01-02 10:43:19 +0900

    commit

--- TIL/aspect_test/src/main/java/kr/co/chunjae/AOP/User.java
+++ TIL/aspect_test/src/main/java/kr/co/chunjae/AOP/User.java
@@ -3,6 +3,7 @@ package kr.co.chunjae.AOP;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @Data
 @NoArgsConstructor
