8429296472d501f3379d21aecdf25977c91bb296
soojin <pofoxz@gmail.com>
2024-02-21 16:40:56 +0900

    Add: getSnapshotBeforeUpdate, componentDidCatch

--- Web_practice/React Basic/lifecycle-react/src/LifeCycle.js
+++ Web_practice/React Basic/lifecycle-react/src/LifeCycle.js
@@ -21,12 +21,33 @@ class LifeCycle extends Component {
         // 현재 들어오는 props 값과 이전의 state 값을 매개변수로 전달받는다.
         // console.log(nextProps, prevState);
 
-        if (nextProps.color != prevState.color) {
+        if (nextProps.color !== prevState.color) {
             // 이전 컬러와 현재 컬러가 다르다면 현재 들어온 props의 color 값을 세팅한다.
             return {color:nextProps.color}
         }
 
         return null;    // 컬러가 동일하다면 null을 반환한다.
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate', prevProps, prevState);  // 이전 color의 'red', state의 'red'가 출력된다.
+
+        // 이전 props의 color와 현재 props의 color가 같지 않으면 이전 props의 color를 반환한다.
+        if (prevProps.color !== this.props.color) {
+            return prevProps.color;
+        }
+
+        return null;
+    }
+
+    // getSnapshotBeforeUpdate에서 반환된 값이 snapshot으로 전달된다.
+    componentDidCatch(prevProps, prevState, snapshot) {
+        console.log('componentDidCatch', prevProps, prevState, snapshot);
+
+        // 이전, 현재 props의 color가 다를 경우 이전 props의 color가 snapshot으로 전달된다.
+        if (snapshot) {
+            console.log("업데이터 직전 색상은? : ", snapshot);
+        }
     }
 
     render() {
