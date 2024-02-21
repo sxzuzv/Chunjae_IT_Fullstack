2047dd4fcb6b1015b9135c0c473287639c8e1305
soojin <pofoxz@gmail.com>
2024-02-21 16:03:14 +0900

    Add: getDerivedStateFromProps 적용

--- Web_practice/React Basic/lifecycle-react/src/LifeCycle.js
+++ Web_practice/React Basic/lifecycle-react/src/LifeCycle.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 
 class LifeCycle extends Component {
     state = {
-        color:this.props.color
+        // color:this.props.color
+
+        // color 값을 초기화한다.
+        color:null
     }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        // 현재 들어오는 props 값과 이전의 state 값을 매개변수로 전달받는다.
+        // console.log(nextProps, prevState);
+
+        if (nextProps.color != prevState.color) {
+            // 이전 컬러와 현재 컬러가 다르다면 현재 들어온 props의 color 값을 세팅한다.
+            return {color:nextProps.color}
+        }
+
+        return null;    // 컬러가 동일하다면 null을 반환한다.
+    }
+
     render() {
         console.log('render');
         return (
