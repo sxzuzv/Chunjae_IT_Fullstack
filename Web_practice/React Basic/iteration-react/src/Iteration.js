356fa8a3d437209414761cb7ba4bfe6c06adc59d
soojin <pofoxz@gmail.com>
2024-02-21 13:10:17 +0900

    Add: key 값 지정

--- Web_practice/React Basic/iteration-react/src/Iteration.js
+++ Web_practice/React Basic/iteration-react/src/Iteration.js
@@ -5,7 +5,10 @@ const Iteration = () => {
     // names를 태그로 변환한다. => .map을 사용한다.
     // names 요소를 하나씩 꺼내서 name(파라미터)에 넣고 함수를 실행한다.
     // names(배열)의 요소를 태그 형태로 변환한다. => 배열 속 값이 단순 문자열에서 태그 형태로 변환된다.
-    const nameList = names.map((name) => <li>{name}</li>);
+
+    // index : javascript는 0, jQuery는 1, React는 2
+    // 각 태그에 대하여 key 값을 넣어준다.
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
     // ['<li>javascript</li>', '<li>jQuery</li>', '<li>React</li>']
     return (
         <ul>
