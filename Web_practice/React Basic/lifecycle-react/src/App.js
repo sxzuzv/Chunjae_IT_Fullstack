d25071a7b2cca57f15b2637490aadfa3e7cc300b
soojin <pofoxz@gmail.com>
2024-02-21 15:57:20 +0900

    Add: props 값 변경

--- Web_practice/React Basic/lifecycle-react/src/App.js
+++ Web_practice/React Basic/lifecycle-react/src/App.js
@@ -11,6 +11,10 @@ class App extends Component {
       <div>
          {/* LifeCycle.js에 color를 props로 내려준다. */}
         <LifeCycle color = {this.state.color} />
+        {/* 버튼 클릭 시, state의 color가 'blue'로 변경되며 내려주는 color가 'blue'로 변경된다. */}
+        <button onClick={() => {
+          this.setState({color:'blue'});
+        }}>BLUE</button>
       </div>
     );
   }
