1291a60d0f777b5bf4dc953ce979db61722b69c5
soojin <pofoxz@gmail.com>
2024-02-21 11:22:19 +0900

    Add: 박스 이동 버튼 추가

--- Web_practice/React Basic/ref-react/src/AppFunction.js
+++ Web_practice/React Basic/ref-react/src/AppFunction.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-// import Validation from 'Validation';
+import React from 'react';
+// import Validation from './Validation';
 import MoveBox from './MoveBox';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
+const App = () => {
+  return (
+    <div>
       {/* <Validation /> */}
       <MoveBox ref={(ref) => {this.MoveBox = ref}} />
       <button onClick={() => {this.MoveBox.move(200, 200)}}>MOVE02</button>
     </div>
-    );
-  }
-}
+  );
+};
 
 export default App;
--- Web_practice/React Basic/ref-react/src/MoveBox.js
+++ Web_practice/React Basic/ref-react/src/MoveBox.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class MoveBox extends Component {
+    // this.box의 left, top 위치를 바꾸는 함수를 작성한다.
+    move = (x, y) => {
+        this.box.style.left = x+'px';
+        this.box.style.top = y+'px';
+    }
     render() {
         const style = {
             border:'1px solid black',
@@ -13,15 +18,21 @@ class MoveBox extends Component {
             width:'100px',
             backgroundColor:'red',
             // 부모 요소의 왼쪽 모서리 기준으로 위치를 이동한다.
-            left:'10px',
+            // left:'100px',
+            // top:'100px',
             position:'absolute'
         }
         return (
-            <div style={style}>
-                <div style={innerstyle}>
+            // <div style={style}>
+            //     <div style={innerstyle} ref={(ref) => {this.box = ref}} />
+            // </div>
 
+            <>
+                <div style={style}>
+                    <div style={innerstyle} ref={(ref) => {this.box = ref}} />
+                    <button onClick={() => {this.move(100, 100)}}>MOVE01</button>
                 </div>
-            </div>
+            </>
         );
     }
 }
