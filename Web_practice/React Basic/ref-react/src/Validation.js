63ccbce249c9a13d20e0db0d520064b959a54a3d
soojin <pofoxz@gmail.com>
2024-02-21 10:37:35 +0900

    Add; input에 ref 적용

--- Web_practice/React Basic/ref-react/src/Validation.js
+++ Web_practice/React Basic/ref-react/src/Validation.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Validation extends Component {
+    handleButtonClick = () => {
+        this.input.focus();
+    }
     render() {
         return (
             <div>
-                <input type='text' name='pwd' />
-                <button>검증</button>
+                {/*  참조를 위해 ref를 사용한다. */}
+                <input type='text' name='pwd' ref={(ref) => {this.input = ref}} />
+                <button onClick={this.handleButtonClick}>검증</button>
             </div>
         );
     }
