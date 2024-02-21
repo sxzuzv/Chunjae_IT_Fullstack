c3eaaeb1f95097c73858aa020e0561aca9ace645
soojin <pofoxz@gmail.com>
2024-02-21 13:07:25 +0900

    Init: 데이터 배열을 컴포넌트 배열로 변환

---
+++ Web_practice/React Basic/iteration-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Iteration from './Iteration';
+
+const App = () => {
+  return (
+    <div>
+      <Iteration />
+    </div>
+  );
+};
+
+export default App;
---
+++ Web_practice/React Basic/iteration-react/src/Iteration.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Iteration = () => {
+    const names = ['javascript', 'jQuery', 'React'];
+    // names를 태그로 변환한다. => .map을 사용한다.
+    // names 요소를 하나씩 꺼내서 name(파라미터)에 넣고 함수를 실행한다.
+    // names(배열)의 요소를 태그 형태로 변환한다. => 배열 속 값이 단순 문자열에서 태그 형태로 변환된다.
+    const nameList = names.map((name) => <li>{name}</li>);
+    // ['<li>javascript</li>', '<li>jQuery</li>', '<li>React</li>']
+    return (
+        <ul>
+            {nameList}
+        </ul>
+    );
+};
+
+export default Iteration;
