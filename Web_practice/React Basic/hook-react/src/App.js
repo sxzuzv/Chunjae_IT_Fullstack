99c64f358f0c161c33edf4b196dd12ee3472fdc9
soojin <pofoxz@gmail.com>
2024-02-22 09:45:08 +0900

    Add: useEffect

--- Web_practice/React Basic/hook-react/src/App.js
+++ Web_practice/React Basic/hook-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Counter from './Counter';
+import Info from './Info';
 
 const App = () => {
   return (
     <div>
+      <Info />
       <Counter />
     </div>
   );
---
+++ Web_practice/React Basic/hook-react/src/Info.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+
+const Info = () => {
+    // useState로 초기값을 설정한다.
+    const [name, setName] = useState('');
+
+    // state 변경 시, useEffect가 실행된다.
+    useEffect(() => {
+        console.log("렌더링 완료");
+        console.log({name});
+    })    
+
+    return (
+        <div>
+            {/* 타이핑으로 인해 값의 변화가 생겼을 때 이벤트를 수행한다. */}
+            {/* input에 대한 오브젝트 정보가 e에 포함돼 있다. */}
+            input : <input type='text' onChange={(e) => setName(e.target.value)} /><br />
+            name : {name}
+        </div>
+    );
+};
+
+export default Info;
