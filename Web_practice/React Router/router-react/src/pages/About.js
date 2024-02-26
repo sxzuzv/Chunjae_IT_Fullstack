a57292de70174c4c6cc6d95087c28b8e9f77480b
soojin <pofoxz@gmail.com>
2024-02-26 14:06:35 +0900

    Add: 프로젝트 소개 페이지 추가

---
+++ Web_practice/React Router/router-react/src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>소개</h1>
+                <p>리액트 라우터를 사용하는 프로젝트입니다.</p>
+            </div>
+        );
+    }
+}
+
+export default About;
