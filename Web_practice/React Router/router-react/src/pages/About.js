49967bf7759b9b0beeee2eb5361b0b2e90dcbff0
soojin <pofoxz@gmail.com>
2024-02-26 14:31:04 +0900

    Fix

--- Web_practice/React Router/router-react/src/pages/About.js
+++ Web_practice/React Router/router-react/src/pages/About.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <h1>소개</h1>
-                <p>리액트 라우터를 사용하는 프로젝트입니다.</p>
-            </div>
-        );
-    }
-}
+const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>리액트 라우터를 사용하는 프로젝트입니다.</p>
+        </div>
+    );
+};
 
 export default About;
--- Web_practice/React Router/router-react/src/pages/Home.js
+++ Web_practice/React Router/router-react/src/pages/Home.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
+const Home = () => {
+    return (
+        <div>
                 <h1>HOME</h1>
                 <p>가장 먼저 보이는 페이지입니다!</p>
                 {/* Link 컴포넌트를 사용해 링크를 설정한다. */}
                 {/* '소개' 클릭 시, 페이지가 전환되나 Request URL은 변경되지 않는다. */}
                 <Link to="/about">소개</Link>
             </div>
-        );
-    }
-}
+    );
+};
 
 export default Home;
