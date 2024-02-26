c93f14ac3f1e649e5f45afc5161208524db9d5a7
soojin <pofoxz@gmail.com>
2024-02-26 14:20:14 +0900

    Add: Route path 추가

--- Web_practice/React Router/router-react/src/App.js
+++ Web_practice/React Router/router-react/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from'react';
+import{Route,Routes}from'react-router-dom';
+import About from'./pages/About';
+import Home from'./pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App=()=>{
+  return(
+    <Routes>
+      {/* URL을 받아오면 에러가 발생한다. SPA는 URL이 바뀌면 안 된다 !!
+      현재 상태는 React 내부적으로 어디로 가야할지를 판단하고 페이지를 띄워준 것이다. */}
+      {/* 주소로 한 번 접속하면 HTML 내려 받고, 이후에는 또 내려 받지 않는다. => SPA의 기본 개념 */}
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
   );
-}
+};
 
 export default App;
