f324a9df98623c2a383716d07f35a0ca323745a5
soojin <pofoxz@gmail.com>
2024-02-26 16:41:46 +0900

    Add: useNavigate 활용 뒤로가기, 게시글 목록 바로가기 버튼 추가

--- Web_practice/React Router/router-react/src/pages/Layout.js
+++ Web_practice/React Router/router-react/src/pages/Layout.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1); // 뒤로가기
+    }
+    const goArticles = () => {
+        navigate('/articles', {replace:true});
+    };
+
     return (
         <div>
             <header style={{background:'lightgray',padding:16,fontSize:24}}>
             header
+                <button onClick={goBack}>뒤로가기</button>
+                <button onClick={goArticles}>게시글 목록 바로가기</button>
             </header>
             <main>
                 {/* 중첩 라우트 설정된 내용이 출력된다. */}
