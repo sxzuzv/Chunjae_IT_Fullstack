b73a15fafb08c55000fcc2be4a31b5aed1161e95
soojin <pofoxz@gmail.com>
2024-02-26 14:06:50 +0900

    Add: 프로젝트 실행 초기 페이지 추가

---
+++ Web_practice/React Router/router-react/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HOME</h1>
+                <p>가장 먼저 보이는 페이지입니다!</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
