31522952b03e2cc4aa84756213a55ad2e46d1a7a
soojin <pofoxz@gmail.com>
2024-02-26 16:30:37 +0900

    Add: NavLink 활용 진입 페이지 스타일 적용

--- Web_practice/React Router/router-react/src/pages/Articles.js
+++ Web_practice/React Router/router-react/src/pages/Articles.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+    const activeStyle = {color:'green', fontSize:21};
+
     return (
         <div>
             {/* Outlet = 중첩된 Article.js의 내용이 함께 출력된다. */}
             <Outlet />
-            <Link to='/articles/1'>게시글 01</Link> |
+            {/* <Link to='/articles/1'>게시글 01</Link> |
             <Link to='/articles/2'>게시글 02</Link> |
-            <Link to='/articles/3'>게시글 03</Link>
+            <Link to='/articles/3'>게시글 03</Link> */}
+
+            {/* 페이지 진입 상황에 따라 지정된 style을 적용한다. */}
+            <NavLink to='/articles/1' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 01</NavLink> |
+            <NavLink to='/articles/2' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 02</NavLink> |
+            <NavLink to='/articles/3' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 03</NavLink>
         </div>
     );
 };
