c0da6efa9eb7f1808bcc4f368cc43d3ebd944e90
soojin <pofoxz@gmail.com>
2024-02-26 10:28:13 +0900

    Add: react-virtualized를 사용한 렌더링 최적화

--- Web_practice/React Basic/todo-app/src/compenents/TodoList.js
+++ Web_practice/React Basic/todo-app/src/compenents/TodoList.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import {List} from 'react-virtualized';
 import './TodoList.css';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({todos,onRemove,onToggle}) => {
+  const rowRender = useCallback(({index, key, style}) => {
+    const todo = todos[index];
+    
+    return (
+      <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style} />
+    );
+  },[todos, onRemove, onToggle]);
   return (
-    <div className='TodoList'>
-      {
-        todos.map((todo) => <TodoListItem onRemove={onRemove} onToggle={onToggle} todo={todo} key={todo.id} />)
-      }
-    </div>
+    <List
+      className='TodoList' width={512} height={513} rowCount={todos.length}
+      rowHeight={57} rowRenderer={rowRender} list={todos} style={{outline:'none'}}
+      />
+    // <div className='TodoList'>
+    //   {
+    //     todos.map((todo) => <TodoListItem onRemove={onRemove} onToggle={onToggle} todo={todo} key={todo.id} />)
+    //   }
+    // </div>
   );
 };
 
--- Web_practice/React Basic/todo-app/src/compenents/TodoListItem.js
+++ Web_practice/React Basic/todo-app/src/compenents/TodoListItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './TodoListItem.css';
 import {MdCheckBoxOutlineBlank,MdCheckBox,MdRemoveCircleOutline} from 'react-icons/md';
-const TodoListItem = ({todo,onRemove,onToggle}) => {
+const TodoListItem = ({todo,onRemove,onToggle,style}) => {
   //console.log(todo);
   const {id,text,checked} = todo; //{id:1,text:'일정1',checked:true}
   return (
-    <div className='TodoListItem'>
+    <div className='TodoListItem-virtualized' style={style}>
       <div className={checked? 'checkbox checked' : 'checkbox'} onClick={() => onToggle(id)}>
         {
           checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />
