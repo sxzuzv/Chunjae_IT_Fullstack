a0521a051af22fe47bae59a7f338adb9941b061c
soojin <pofoxz@gmail.com>
2024-02-27 14:25:31 +0900

    Add: 라우터 적용 페이지 레이아웃 설정

--- Web_practice/React API/news-viewer-react/src/App.js
+++ Web_practice/React API/news-viewer-react/src/App.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
-import axios from '../node_modules/axios/index';
+import React from 'react';
+import Layout from './components/Layout';
+import { Route, Routes } from '../node_modules/react-router-dom/dist/index';
+import Home from './components/Home';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    // axios : 응답이 오면 알려준다.
-    // axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {setData(response)});
-
-    // async, News Api 적용
-    const response=await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=e3aabbbc9e0148af8f9574ddc1ba0467');
-    setData(response);
-  };
-
   return (
-    <div>
-      <button onClick={onClick}>불러오기</button>
-      {/* {data}로 출력하려고 하면 오류가 발생한다. Object type은 display 불가하므로 문자로 변환이 필요하다. */}
-      <div>
-        {data && <textarea value = {JSON.stringify(data, null, 2)} />}
-      </div>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        {/* 하위에 들어오는 경로는 Layout에 지정된 대로 출력한다. */}
+        <Route path='/' element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
 
--- Web_practice/React API/news-viewer-react/src/components/Layout.js
+++ Web_practice/React API/news-viewer-react/src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
+import { Outlet } from '../../node_modules/react-router-dom/dist/index';
 
 const Layout = () => {
     // 세부 내용은 컴포넌트로 나눴고, 각 컴포넌트와 함께 전체 레이아웃을 잡아준다.
@@ -10,7 +11,8 @@ const Layout = () => {
             <Header />
             <div className="container">
                 <main role="main">
-                    <Home />
+                    {/* Route 컴포넌트 설정에 따라 콘텐츠가 출력된다. */}
+                    <Outlet />
                 </main>
                 <Footer />
             </div>
