04bb264fb03862485432851764cd25fe765e80a7
soojin <pofoxz@gmail.com>
2024-02-27 16:33:22 +0900

    Add: 카테고리 선택에 따른 뉴스 리스트 출력

--- Web_practice/React API/news-viewer-react/src/components/News.js
+++ Web_practice/React API/news-viewer-react/src/components/News.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import NewsList from './NewsList';
 
+// 클릭한 메뉴에 맞는 리스트를 출력한다.
 const News = () => {
+    const [category, setCategory] = useState('all');
+
+    const onselect = useCallback((e, category) => {
+        // 값을 세팅해준다.
+        setCategory(category);
+        
+        // 이중 클릭을 방지한다.
+        e.preventDefault();
+    }, []
+    );
+    
     return (
         <section>
             <ul className="nav nav-tabs">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a onClick={(e) => onselect(e, 'all')} className={category == 'all' ? 'nav-link active' : 'nav-link'} aria-current="page" href="#">
                         전체보기
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">
+                    <a onClick={(e) => onselect(e, 'business')} className="nav-link active" aria-current="page" href="#">
                         비즈니스
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">
+                    <a onClick={(e) => onselect(e, 'entertainment')} className="nav-link active" aria-current="page" href="#">
                         엔터테인먼트
                     </a>
                 </li>
@@ -25,7 +37,9 @@ const News = () => {
                     <div className="container">
                         <div className="row">
                             {/* 뉴스 리스트를 출력한다. */}
-                            <NewsList />
+                            {/* 카테고리 정보를 NewsList에 props로 내려준다. */}
+                            {/* 내려주면서 렌더링이 다시 걸린다. */}
+                            <NewsList category={category} />
                         </div>
                     </div>
                 </div>
--- Web_practice/React API/news-viewer-react/src/components/NewsList.js
+++ Web_practice/React API/news-viewer-react/src/components/NewsList.js
@@ -3,7 +3,8 @@ import NewsItem from './NewsItem';
 import axios from '../../node_modules/axios/index';
 
 // NewsItem을 가지고 있는 NewsList 컴포넌트
-const NewsList = () => {
+// News 컴포넌트에서 내려준 category를 받는다.
+const NewsList = ({category}) => {
     // Api를 사용해서 뉴스 데이터를 받아온 후,
     // props를 사용하여 NewsItem 컴포넌트에 하나씩 내려준다.
 
@@ -14,8 +15,14 @@ const NewsList = () => {
         () => {
         // fetchData() 실행되면 Api 데이터를 받아온다.
         const fetchData = async () => {
-            const response = await axios.get(
-                'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e3aabbbc9e0148af8f9574ddc1ba0467');
+            // props로 내려온 category가 '전체보기'일 경우, url을 유지한다.
+            // 그렇지 않을 경우, 쿼리스트링을 이용하여 url을 변경한다.
+            const query = category == 'all' ? '' : '&category='+category;
+
+            const apiUrl = 'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e3aabbbc9e0148af8f9574ddc1ba0467';
+
+            // url에 생성한 query를 붙여준다.
+            const response = await axios.get(apiUrl+query);
             
             // axios가 가져온 데이터는 data에 담겨져 있다.
             // data의 어떤 내용을 가지고 오고 싶은지를 지정해주면 된다. (현재는 articles에 필요 데이터가 담겨있다.)
@@ -25,7 +32,7 @@ const NewsList = () => {
             setArticles(response.data.articles);
         };
         fetchData(); // 마운트 걸리면 fetchData() 실행된다.
-    }, []
+    }, [category]
     );
 
     // 데이터가 들어오면 state가 바뀌게 되고 리렌더링이 진행된다.
