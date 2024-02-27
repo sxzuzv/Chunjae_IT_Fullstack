3882df709829d3afd268b4d5f27576b8ed1a78c4
soojin <pofoxz@gmail.com>
2024-02-27 14:49:59 +0900

    Add: Header 컴포넌트 NavLink 적용

--- Web_practice/React API/news-viewer-react/src/components/Header.js
+++ Web_practice/React API/news-viewer-react/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from '../../node_modules/react-router-dom/dist/index';
 
 // 페이지 header를 설계한다.
 // 최종적으로 <div> 태그 하나만을 감싸져 있으므로, 변환(HTML->JSX)한 코드를 그대로 사용한다.
@@ -7,8 +8,12 @@ const Header = () => {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 className="my-0 mr-md-auto font-weight-bold">News API</h5>
             <nav className="my-2 my-md-0 mr-md-3">
-                <a className="p-2 text-dark active" href="#">HOME</a>
-                <a className="p-2 text-dark" href="#">뉴스</a>
+                {/* <a className="p-2 text-dark active" href="#">HOME</a>
+                <a className="p-2 text-dark" href="#">뉴스</a> */}
+
+                {/* SPA : <a> 태그 적용 불가, NavLink를 사용하여 Request URL을 유지하면서 페이지는 변경한다. */}
+                <NavLink className="p-2 text-dark active" to="/">HOME</NavLink>
+                <NavLink className="p-2 text-dark active" to="/news">NEWS</NavLink>
             </nav>
     </div>
     );
